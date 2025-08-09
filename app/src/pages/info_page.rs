b8d4dd4927157bc7ff3a8aeb06b9cab1a2b2e357
idzeir <qiyanlong@wozine.com>
2025-08-09 19:16:51 +0800

    feat: ui details modify

--- app/src/pages/info_page.rs
+++ app/src/pages/info_page.rs
@@ -8,11 +8,11 @@ import_crate_style!(css, "src/pages/info_page.module.scss");
 
 cfg_block! {
     #[cfg(feature="ssr")] {
-        use leptos::logging::log;
+        //use leptos::logging::log;
         use std::time::Duration;
         use tokio::time::sleep;
         use axum::http::request::Parts;
-        use axum::http::header::CONTENT_TYPE;
+        //use axum::http::header::CONTENT_TYPE;
     }
 }
 
@@ -20,8 +20,8 @@ cfg_block! {
 #[lazy]
 async fn get_rows() -> Result<usize, ServerFnError> {
     let req_parts = use_context::<Parts>();
-    if let Some(req_parts) = req_parts {
-        log!("GET ROW: {:?}", req_parts.headers.get(CONTENT_TYPE));
+    if let Some(_req_parts) = req_parts {
+        // log!("GET ROW: {:?}", req_parts.headers.get(CONTENT_TYPE));
     }
     sleep(Duration::from_secs(2)).await;
     Ok(888)
@@ -29,7 +29,7 @@ async fn get_rows() -> Result<usize, ServerFnError> {
 
 #[server]
 async fn add_row(text: String) -> Result<usize, ServerFnError> {
-    log!("ADD ROW -> {}", text);
+    // log!("ADD ROW -> {}", text);
     sleep(Duration::from_secs(2)).await;
 
     if text == *"Error" {
@@ -77,7 +77,8 @@ pub fn InfoPage() -> impl IntoView {
                 <Transition fallback=|| "">
                     <p class=css::result>Total rows: {size}</p>
                 </Transition>
-            </div> <SimpleQueryCounter />
+            </div>
+            <SimpleQueryCounter />
         </div>
     }
 }
